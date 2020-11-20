14be73355a6b4cb7b73a4ae4ccc29c5aaa34384f
Leonkh <62510482+Leonkh@users.noreply.github.com>
2020-11-20 22:57:26 +0300

    Добавил иконку приложения, поменял вывод данных на экран

--- WeatherChecker/WeatherChecker/ViewController.swift
+++ WeatherChecker/WeatherChecker/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController, UITextViewDelegate {
     var cityName: String?
     var apiKey: String!
     var jsonData: JSON?
+    var weatherParam = [String: String]()
     
     @IBOutlet var textView: UITextView!
     @IBOutlet var searchBar: UISearchBar!
@@ -26,6 +27,8 @@ class ViewController: UIViewController, UITextViewDelegate {
         searchBar.delegate = self
         textView.delegate = self
         apiKey = "4a4e219d6b73a256ed2379f35ecbaeb9"
+        
+       
     }
     
     
@@ -34,20 +37,34 @@ class ViewController: UIViewController, UITextViewDelegate {
     
     func getWeatherInfo(cityName: String) {
         let url = "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&lang=ru&units=metric&appid=\(apiKey!)"
-        AF.request(url, method: .get).validate().responseJSON { (response) in
+        AF.request(url, method: .get).validate().responseJSON { [weak self] (response) in
             switch response.result {
             case .success(let value):
                 let json = JSON(value)
-                self.jsonData = json
-                self.textView.text = self.jsonData?.description
-                print("В замыкании: \(self.jsonData)")
-                print("В замыкании: \(self.jsonData?.description)")
+                self?.jsonData = json
+                self?.textView.text = self?.jsonData?.description
+                self?.weatherParam = [
+                    "Название города": self!.jsonData!["name"].stringValue,
+                    "Температура в цельсиях": self!.jsonData!["main"]["temp"].stringValue,
+                    "Долгота": self!.jsonData!["coord"]["lat"].stringValue,
+                    "Широта": self!.jsonData!["coord"]["lon"].stringValue,
+                    "Описание состояния на улице": self!.jsonData!["weather"]["description"].stringValue,
+                    "Скорость ветра в м/с": self!.jsonData!["wind"]["speed"].stringValue,
+                    "Влажность в %": self!.jsonData!["main"]["humidity"].stringValue
+                ]
+                self?.textView.text = """
+                    Название города:  \(self!.weatherParam["Название города"]!)
+                    Температура: \(self!.weatherParam["Температура в цельсиях"]!) градусов цельсия
+                    Долгота: \(self!.weatherParam["Долгота"]!)
+                    Широта: \(self!.weatherParam["Широта"]!)
+                    Сейчас на улице \(self!.weatherParam["Описание состояния на улице"]!)
+                    Скорость ветра \(self!.weatherParam["Скорость ветра в м/с"]!) м/с
+                    Влажность \(self!.weatherParam["Влажность в %"]!)%
+                    """
             case .failure(let error):
                 print(error)
             }
         }
-//        print("Вне замыкания: \(jsonData)")
-        
     }
     
     @objc func dismissKeyboard() {
