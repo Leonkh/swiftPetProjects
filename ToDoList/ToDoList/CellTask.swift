0d539447d70eb35da603b44864bb954ff9b0ce79
Leonkh <62510482+Leonkh@users.noreply.github.com>
2020-11-23 16:56:14 +0300

    Улучшенно введение города

---
+++ ToDoList/ToDoList/CellTask.swift
@@ -0,0 +1,15 @@
+//
+//  Cell.swift
+//  ToDoList
+//
+//  Created by Леонид Хабибуллин on 22.11.2020.
+//
+
+import Foundation
+import UIKit
+
+class CellTask: UITableViewCell {
+    var nameOfTask: String?
+    var status: Bool = false
+    var textOfTask: String?
+}
